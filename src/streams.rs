ceb749d0f4f994584bf77af5174405b9d6d25403
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-07-25 13:12:46 -0400

    made send

--- src/streams.rs
+++ src/streams.rs
@@ -5,14 +5,18 @@ use futures::{Future, Stream};
 /// `eth_subscribe`
 pub trait EthStream {
     /// `newHeads`
-    fn block_stream(&self) -> impl Future<Output = eyre::Result<impl Stream<Item = Block> + Send>>;
+    fn block_stream(&self) -> impl Future<Output = eyre::Result<impl Stream<Item = Block> + Send>> + Send;
 
     /// `newPendingTransactions` (true)
-    fn full_pending_transaction_stream(&self) -> impl Future<Output = eyre::Result<impl Stream<Item = Transaction> + Send>>;
+    fn full_pending_transaction_stream(
+        &self
+    ) -> impl Future<Output = eyre::Result<impl Stream<Item = Transaction> + Send>> + Send;
 
     /// `newPendingTransactions` (false)
-    fn pending_transaction_hashes_stream(&self) -> impl Future<Output = eyre::Result<impl Stream<Item = TxHash> + Send>>;
+    fn pending_transaction_hashes_stream(
+        &self
+    ) -> impl Future<Output = eyre::Result<impl Stream<Item = TxHash> + Send>> + Send;
 
     /// `logs`
-    fn log_stream(&self, filter: &Filter) -> impl Future<Output = eyre::Result<impl Stream<Item = Log> + Send>>;
+    fn log_stream(&self, filter: &Filter) -> impl Future<Output = eyre::Result<impl Stream<Item = Log> + Send>> + Send;
 }
