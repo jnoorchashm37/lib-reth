8be7d1033d1ed27c2b15dd7f86d4c8e3eb7c5567
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-11-03 17:30:58 -0500

    made libmdbx builder

--- src/libmdbx.rs
+++ src/libmdbx.rs
@@ -57,25 +57,19 @@ type RethTxPool = Pool<
 >;
 
 #[derive(Debug, Clone)]
-pub struct RethLibmdbxClientBuilder<T = TokioTaskExecutor> {
-    db_path:       String,
-    max_tasks:     usize,
-    db_args:       Option<DatabaseArguments>,
-    task_executor: Option<T>
+pub struct RethLibmdbxClientBuilder {
+    db_path:   String,
+    max_tasks: usize,
+    db_args:   Option<DatabaseArguments>
 }
 
-impl<T: TaskSpawner + Clone + 'static> RethLibmdbxClientBuilder<T> {
+impl RethLibmdbxClientBuilder {
     pub fn new(db_path: &str, max_tasks: usize) -> Self {
-        Self { db_path: db_path.to_string(), max_tasks, db_args: None, task_executor: None }
+        Self { db_path: db_path.to_string(), max_tasks, db_args: None }
     }
 
     pub fn with_db_args(mut self, db_args: DatabaseArguments) -> Self {
         self.db_args = Some(db_args);
-        self
-    }
-
-    pub fn with_task_executor(mut self, task_executor: T) -> Self {
-        self.task_executor = Some(task_executor);
         self
     }
 
@@ -90,11 +84,24 @@ impl<T: TaskSpawner + Clone + 'static> RethLibmdbxClientBuilder<T> {
         static_files.pop();
         static_files.push("static_files");
 
-        if let Some(executor) = self.task_executor {
-            new_with_db(db, self.max_tasks, executor, static_files)
-        } else {
-            new_with_db(db, self.max_tasks, TokioTaskExecutor::default(), static_files)
-        }
+        new_with_db(db, self.max_tasks, TokioTaskExecutor::default(), static_files)
+    }
+
+    pub fn build_with_task_executor<T: TaskSpawner + Clone + 'static>(
+        self,
+        task_executor: T
+    ) -> eyre::Result<RethLibmdbxClient> {
+        let db_path = Path::new(&self.db_path);
+        let db = Arc::new(open_db_read_only(
+            db_path,
+            self.db_args
+                .unwrap_or(DatabaseArguments::new(Default::default()))
+        )?);
+        let mut static_files = db_path.to_path_buf();
+        static_files.pop();
+        static_files.push("static_files");
+
+        new_with_db(db, self.max_tasks, task_executor, static_files)
     }
 }
 
