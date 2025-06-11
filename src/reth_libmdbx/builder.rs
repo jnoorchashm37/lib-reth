3b42f7c6d0513be088d21521d69d3066dec3963a
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-06-11 19:07:45 -0400

    made provider

--- src/reth_libmdbx/builder.rs
+++ src/reth_libmdbx/builder.rs
@@ -3,21 +3,24 @@ use std::{
     sync::Arc,
 };
 
+use alloy_network::Ethereum;
+use alloy_provider::{Provider, RootProvider};
 use reth_db::{mdbx::DatabaseArguments, open_db_read_only};
 use reth_tasks::{TaskSpawner, TokioTaskExecutor};
 
 use crate::reth_libmdbx::RethLibmdbxClient;
 
 #[derive(Debug, Clone)]
-pub struct RethLibmdbxClientBuilder {
+pub struct RethLibmdbxClientBuilder<P> {
     db_path: String,
     max_tasks: usize,
     db_args: Option<DatabaseArguments>,
+    provider: P,
 }
 
-impl RethLibmdbxClientBuilder {
-    pub fn new(db_path: &str, max_tasks: usize) -> Self {
-        Self { db_path: db_path.to_string(), max_tasks, db_args: None }
+impl<P: Provider> RethLibmdbxClientBuilder<P> {
+    pub fn new(db_path: &str, max_tasks: usize, provider: P) -> Self {
+        Self { provider, db_path: db_path.to_string(), max_tasks, db_args: None }
     }
 
     pub fn with_db_args(mut self, db_args: DatabaseArguments) -> Self {
@@ -25,7 +28,7 @@ impl RethLibmdbxClientBuilder {
         self
     }
 
-    pub fn build(self) -> eyre::Result<RethLibmdbxClient> {
+    pub fn build(self) -> eyre::Result<RethLibmdbxClient<P>> {
         let (db_path, static_files) = self.db_paths()?;
 
         let db = Arc::new(open_db_read_only(
@@ -34,13 +37,13 @@ impl RethLibmdbxClientBuilder {
                 .unwrap_or(DatabaseArguments::new(Default::default())),
         )?);
 
-        crate::reth_libmdbx::init::new_with_db(db, self.max_tasks, TokioTaskExecutor::default(), static_files)
+        crate::reth_libmdbx::init::new_with_db(db, self.max_tasks, TokioTaskExecutor::default(), static_files, self.provider)
     }
 
     pub fn build_with_task_executor<T: TaskSpawner + Clone + 'static>(
         self,
         task_executor: T,
-    ) -> eyre::Result<RethLibmdbxClient> {
+    ) -> eyre::Result<RethLibmdbxClient<P>> {
         let (db_path, static_files) = self.db_paths()?;
 
         let db = Arc::new(open_db_read_only(
@@ -49,7 +52,7 @@ impl RethLibmdbxClientBuilder {
                 .unwrap_or(DatabaseArguments::new(Default::default())),
         )?);
 
-        crate::reth_libmdbx::init::new_with_db(db, self.max_tasks, task_executor, static_files)
+        crate::reth_libmdbx::init::new_with_db(db, self.max_tasks, task_executor, static_files, self.provider)
     }
 
     /// (db_path, static_files)
@@ -68,5 +71,16 @@ impl RethLibmdbxClientBuilder {
         }
 
         Ok((db_path, static_files_path))
+    }
+}
+
+impl RethLibmdbxClientBuilder<RootProvider<Ethereum>> {
+    pub fn new_no_root_provider(db_path: &str, max_tasks: usize) -> Self {
+        let provider = alloy_provider::ProviderBuilder::<_, _, alloy_network::Ethereum>::default();
+        RethLibmdbxClientBuilder::new(
+            db_path,
+            max_tasks,
+            provider.connect_mocked_client(alloy_provider::mock::Asserter::new()),
+        )
     }
 }
--- src/reth_libmdbx/mod.rs
+++ src/reth_libmdbx/mod.rs
@@ -2,6 +2,7 @@ mod init;
 
 use alloy_consensus::TxEnvelope;
 use alloy_primitives::{TxHash, U256};
+use alloy_provider::Provider;
 use alloy_rpc_types::{
     eth::{Filter, Log},
     Header,
@@ -20,16 +21,17 @@ pub use builder::*;
 use crate::traits::EthStream;
 
 /// direct libmdbx database connection to a reth node
-pub struct RethLibmdbxClient {
+pub struct RethLibmdbxClient<P: Provider> {
     api: RethApi,
     filter: RethFilter,
     trace: RethTrace,
     debug: RethDebug,
     tx_pool: RethTxPool,
     db_provider: RethDbProvider,
+    root_provider: P,
 }
 
-impl RethLibmdbxClient {
+impl<P: Provider> RethLibmdbxClient<P> {
     pub fn eth_api(&self) -> RethApi {
         self.api.clone()
     }
@@ -53,9 +55,13 @@ impl RethLibmdbxClient {
     pub fn eth_db_provider(&self) -> &RethDbProvider {
         &self.db_provider
     }
+
+    pub fn root_provider(&self) -> &P {
+        &self.root_provider
+    }
 }
 
-impl EthStream for RethLibmdbxClient {
+impl<P: Provider> EthStream for RethLibmdbxClient<P> {
     async fn block_stream(&self) -> eyre::Result<impl Stream<Item = alloy_rpc_types_eth::Header> + Send> {
         let stream = self
             .api
@@ -146,7 +152,7 @@ impl EthStream for RethLibmdbxClient {
 }
 
 #[cfg(feature = "revm")]
-impl crate::traits::EthRevm for RethLibmdbxClient {
+impl<P: Provider> crate::traits::EthRevm for RethLibmdbxClient<P> {
     type InnerDb = crate::traits::reth_revm_utils::RethLibmdbxDatabaseRef;
 
     fn make_inner_db(&self, block_number: u64) -> eyre::Result<Self::InnerDb> {
@@ -173,14 +179,14 @@ mod tests {
     #[tokio::test]
     #[serial_test::serial]
     async fn can_build() {
-        let builder = RethLibmdbxClientBuilder::new("/home/data/reth/db", 1000);
+        let builder = RethLibmdbxClientBuilder::new_no_root_provider("/home/data/reth/db", 1000);
         assert!(builder.build().is_ok())
     }
 
     #[tokio::test(flavor = "multi_thread")]
     #[serial_test::serial]
     async fn can_stream() {
-        let builder = RethLibmdbxClientBuilder::new("/home/data/reth/db", 1000);
+        let builder = RethLibmdbxClientBuilder::new_no_root_provider("/home/data/reth/db", 1000);
         let client = builder.build().unwrap();
 
         // let block_stream = client.block_stream().await.unwrap();
