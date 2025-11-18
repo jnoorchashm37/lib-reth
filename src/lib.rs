52229ccfe544e8ebcb869b164fa686cf7d8b8823
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-11-18 13:25:46 -0500

    updated deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(ambiguous_glob_reexports)]
+
 #[cfg(feature = "reth-libmdbx-revm")]
 pub mod reth_libmdbx;
 
@@ -6,7 +8,13 @@ pub mod traits;
 
 #[cfg(feature = "reth-libmdbx-revm")]
 pub use reth_chainspec::*;
+
 #[cfg(feature = "reth-libmdbx-revm")]
 pub use reth_rpc_eth_api::*;
 #[cfg(feature = "reth-libmdbx-revm")]
 pub use revm::*;
+
+#[cfg(feature = "op-reth")]
+pub mod op_reth {
+    pub use reth_optimism_chainspec::*;
+}
