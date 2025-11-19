6899533c11e03d5bfd6cb7cb17a671c391e66291
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-11-18 19:24:33 -0500

    updated with utils

---
+++ src/dual_provider.rs
@@ -0,0 +1,36 @@
+use std::{marker::PhantomData, sync::Arc};
+
+use alloy_network::Network;
+use alloy_provider::Provider;
+
+use crate::reth_libmdbx::{NodeClientSpec, RethNodeClient};
+
+pub struct DualRethNodeClient<Node, P, N>
+where
+    Node: NodeClientSpec,
+    P: Provider<N> + Clone,
+    N: Network,
+{
+    db_client: Arc<RethNodeClient<Node>>,
+    rpc_provider: P,
+    _phantom: PhantomData<N>,
+}
+
+impl<Node, P, N> DualRethNodeClient<Node, P, N>
+where
+    Node: NodeClientSpec,
+    P: Provider<N> + Clone,
+    N: Network,
+{
+    pub fn new(db_client: Arc<RethNodeClient<Node>>, rpc_provider: P) -> Self {
+        Self { db_client, rpc_provider, _phantom: PhantomData }
+    }
+
+    pub fn replace_rpc_provider(&mut self, rpc_provider: P) {
+        self.rpc_provider = rpc_provider;
+    }
+
+    pub fn db_client(&self) -> Arc<RethNodeClient<Node>> {
+        self.db_client.clone()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,9 @@ pub use reth_rpc_eth_api::*;
 #[cfg(feature = "reth-libmdbx")]
 pub use revm::*;
 
+mod dual_provider;
+pub use dual_provider::*;
+
 #[cfg(feature = "op-reth-libmdbx")]
 pub mod op_reth {
     pub use reth_optimism_chainspec::*;
